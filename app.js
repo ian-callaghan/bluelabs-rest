042a26965681a49026b91627692ef2dbe97b85ba
Ian Callaghan <33469816+ian-callaghan@users.noreply.github.com>
2020-06-24 12:25:31 +0100

    cors crap

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use("/loginmethods", loginMethodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   next(createError(404));
 });
 
